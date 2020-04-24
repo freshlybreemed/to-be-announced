f31b06912369ac904d8353652ac22aa64d08f5b0
Ebrima <ebrima.jobe92@gmail.com>
2020-04-24 04:03:03 -0700

    nav login logic

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,34 +1,54 @@
-import * as React from "react";
-
-export const Nav: React.FunctionComponent = () => (
-  <div className="overflow-hidden  mb5">
-    <a className="fl fr-ns no-underline gray f3 fw6" href="/">
-      TBA{" "}
-    </a>
-    <ul className=" fr f3 db pa0 ma0 ">
-      <li className="fl tr mr2 list">
-        <a
-          className="fl dib dim no-underline white "
-          target="_blank"
-          href="/events"
-        >
-          Events
-        </a>
-      </li>
-      <li className="fl tr mr2 list">
-        <a
-          className="fl dib dim no-underline white "
-          target="_blank"
-          href="/about"
-        >
-          About
-        </a>
-      </li>
-      <li className="fl tr mr2 list ">
-        <a className="fl dib dim no-underline white " href="/login">
-          Login
-        </a>
-      </li>
-    </ul>
-  </div>
-);
+import * as React from 'react';
+import { getCookieFromBrowser, removeCookie } from '../lib';
+import Router from 'next/router';
+export const Nav: React.FunctionComponent = () => {
+  const isLoggedIn = getCookieFromBrowser('id_token') ? true : false;
+  const handleLogout = () => {
+    removeCookie('id_token');
+    removeCookie('id_token_a');
+    Router.push('/');
+  };
+  return (
+    <div className="overflow-hidden  mb5">
+      <a className="fl fr-ns no-underline gray f3 fw6" href="/">
+        TBA{' '}
+      </a>
+      <ul className=" fr f3 db pa0 ma0 ">
+        <li className="fl tr mr2 list">
+          <a
+            className="fl dib dim no-underline white "
+            target="_blank"
+            href="/events"
+          >
+            Events
+          </a>
+        </li>
+        <li className="fl tr mr2 list">
+          <a
+            className="fl dib dim no-underline white "
+            target="_blank"
+            href="/about"
+          >
+            About
+          </a>
+        </li>
+        {isLoggedIn ? (
+          <li className="fl tr mr2 list ">
+            <a
+              onClick={handleLogout}
+              className="fl dib dim no-underline white "
+            >
+              Logout
+            </a>
+          </li>
+        ) : (
+          <li className="fl tr mr2 list ">
+            <a className="fl dib dim no-underline white " href="/login">
+              Login
+            </a>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
