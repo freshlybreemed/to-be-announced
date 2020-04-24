8a7f2641cd3fa096ea67629f81fd1e19ad310326
ebrima <ebrima.jobe92@gmail.com>
2020-04-24 16:28:08 -0700

    firebase configs

--- src/lib/firebase/config.js
+++ src/lib/firebase/config.js
@@ -1,23 +1,12 @@
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: 'clouty-a1d49.firebaseapp.com',
-  databaseURL: 'https://clouty-a1d49.firebaseio.com',
-  projectId: 'clouty-a1d49',
-  storageBucket: 'clouty-a1d49.appspot.com',
-  messagingSenderId: '291953120378',
+  authDomain: "tba-platform.firebaseapp.com",
+  databaseURL: "https://tba-platform.firebaseio.com",
+  projectId: "tba-platform",
+  storageBucket: "tba-platform.appspot.com",
+  messagingSenderId: "744460150181",
   appId: process.env.FIREBASE_APP_ID,
-  measurementId: 'G-5H9NG41QGZ',
+  measurementId: "G-ZVNZC9DCSE",
 };
 
-const firebaseDevConfig = {
-  apiKey: process.env.FIREBASE_API_KEY_DEV,
-  authDomain: 'clouty-dev-db87b.firebaseapp.com',
-  databaseURL: 'https://clouty-dev-db87b.firebaseio.com',
-  projectId: 'clouty-dev-db87b',
-  storageBucket: 'clouty-dev-db87b.appspot.com',
-  messagingSenderId: '936417933277',
-  appId: process.env.FIREBASE_APP_ID_DEV,
-  measurementId: 'G-GQE044KGQ4',
-};
-
-export { firebaseConfig, firebaseDevConfig };
+export { firebaseConfig };
