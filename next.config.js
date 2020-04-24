d222aa6604484c6e319b67a34efba75d31ac783d
Ebrima <ebrima.jobe92@gmail.com>
2020-04-24 04:02:02 -0700

    add firebase keys

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-const withCSS = require("@zeit/next-css");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const withCSS = require('@zeit/next-css');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = withCSS({
   webpack(config) {
     if (process.env.ANALYZE) {
       config.plugins.push(
         new BundleAnalyzerPlugin({
-          analyzerMode: "server",
+          analyzerMode: 'server',
           analyzerPort: 8888,
           openAnalyzer: true,
-        })
+        }),
       );
     }
     return config;
@@ -18,5 +18,7 @@ module.exports = withCSS({
 
   env: {
     MONGO_URL: process.env.MONGO_URL,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
   },
 });
