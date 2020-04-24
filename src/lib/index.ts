a5a602421497e2349ba7ea1d54fc039336a55565
ebrima <ebrima.jobe92@gmail.com>
2020-04-24 02:03:33 -0700

    formatDate and formatTime helpers

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 import { format } from "date-fns";
 
-export const formatDate = (date: Date) => format(date, "MMMM d, yyyy");
+export const formatDate = (date: Date) => format(date, " ccc. MMMM d, yyyy");
+
+export const formatTime = (date: Date) => format(date, "h:mm a");
