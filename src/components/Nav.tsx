aa74ccbc765dc29276bce7f9a0042b8bd51fb576
Ebrima <ebrima.jobe92@gmail.com>
2020-04-25 21:31:46 -0700

    nav update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,34 +1,25 @@
-import * as React from "react";
-import { getCookieFromBrowser, removeCookie } from "../lib";
-import Router from "next/router";
+import * as React from 'react';
+import { getCookieFromBrowser, removeCookie } from '../lib';
+import Router from 'next/router';
 export const Nav: React.FunctionComponent = () => {
-  const isLoggedIn = getCookieFromBrowser("id_token") ? true : false;
+  const isLoggedIn = getCookieFromBrowser('id_token') ? true : false;
   const handleLogout = () => {
-    removeCookie("id_token");
-    removeCookie("id_token_a");
-    Router.push("/");
+    removeCookie('id_token');
+    Router.push('/');
   };
   return (
     <div className="overflow-hidden  mb5">
       <a className="fl fr-ns no-underline gray f3 fw6" href="/">
-        TBA{" "}
+        <span className="sc-683qsc-1 gyQBaO">TBA</span>
       </a>
       <ul className=" fr f3 db pa0 ma0 ">
         <li className="fl tr mr3 list">
-          <a
-            className="fl dib dim no-underline white "
-            target="_blank"
-            href="/events"
-          >
+          <a className="fl dib dim no-underline white " href="/events">
             Events
           </a>
         </li>
         <li className="fl tr mr3 list">
-          <a
-            className="fl dib dim no-underline white "
-            target="_blank"
-            href="/about"
-          >
+          <a className="fl dib dim no-underline white " href="/about">
             About
           </a>
         </li>
