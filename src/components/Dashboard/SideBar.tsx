76a08499e24ecdc1eced6ae91f2cea9d3f157313
ebrima <ebrima.jobe92@gmail.com>
2020-04-26 00:43:20 -0700

    sidebar component

---
+++ src/components/Dashboard/SideBar.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+
+export const SideBar: React.FunctionComponent = () => (
+  <nav className="w-100 w-25-m w-25-l mb4 mb0-l ph3-m ph3-l">
+    <header className="mb2">
+      <i className="material-icons f2 black-70">dashboard</i>
+    </header>
+    <h2 className="ttu mt0 mb2 f5 fw5 white">Manage</h2>
+    <ul className="list pl0 mt0 mb4 f4">
+      <li className="mb2">
+        <a href="/dashboard/create" className="block link dim gray">
+          Create An Event
+        </a>
+      </li>
+      <li className="mb2">
+        <a href="#" className="block link dim gray">
+          Events
+        </a>
+      </li>
+      <li className="mb2">
+        <a href="#" className="block link dim gray">
+          Payouts
+        </a>
+      </li>
+    </ul>
+    <h2 className="ttu mt0 mb2 f5 fw5 white">More</h2>
+    <ul className="list pl0 mt0 f4 mb2">
+      <li className="mb2">
+        <a href="#" className="block link dim gray">
+          Bank Settings
+        </a>
+      </li>
+      <li className="mb2">
+        <a href="#" className="block link dim gray">
+          Organizer Profile
+        </a>
+      </li>
+      <li className="mb2">
+        <a href="#" className="block link dim gray">
+          Account Settings
+        </a>
+      </li>
+      <li>
+        <a href="#" className="block link dim gray">
+          Log out
+        </a>
+      </li>
+    </ul>
+  </nav>
+);
