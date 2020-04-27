ed183de313fdc910bbf6879e71742a17e6f0650d
ebrima <ebrima.jobe92@gmail.com>
2020-04-27 01:56:06 -0700

    tierra whack jest test

--- src/components/Home.spec.tsx
+++ src/components/Home.spec.tsx
@@ -1,16 +1,14 @@
-import * as React from 'react'
-import {shallow} from 'enzyme'
+import * as React from "react";
+import { shallow } from "enzyme";
 
-import {Home} from './Home'
+import { Home } from "./Home";
 
-describe('Component', () => {
-  describe('Home', () => {
-    it('should render without throwing an error', function () {
-      expect(shallow(<Home/>).contains(
-        <li>
-          usage classnames in Home.tsx
-        </li>
-      )).toEqual(true)
-    })
-  })
-})
+describe("Component", () => {
+  describe("Home", () => {
+    it("should render without throwing an error", function () {
+      expect(
+        shallow(<Home />).contains(<h3 className="fw5 f3 mb0">Tierra Whack</h3>)
+      ).toEqual(true);
+    });
+  });
+});
