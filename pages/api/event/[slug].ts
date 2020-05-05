3ca51e75df60a48ce09b9acaac947abe7951ae34
ebrima <ebrima.jobe92@gmail.com>
2020-05-05 11:38:05 -0700

    clean up

--- pages/api/event/[slug].ts
+++ pages/api/event/[slug].ts
@@ -1,13 +1,6 @@
 import { wrapAsync } from '../helpers';
 import { NextApiRequest } from 'next';
-// const ObjectId = require("mongodb").ObjectId;
 
-export default wrapAsync(
-  async (req: NextApiRequest, db: any) =>
-    // !ObjectId.isValid(req.query.slug)
-    db.collection('tba').find({ slug: req.query.slug }).toArray(),
-  // : db
-  //     .collection("tba")
-  //     .find({ _id: ObjectId(req.query.slug) })
-  //     .toArray()
+export default wrapAsync(async (req: NextApiRequest, db: any) =>
+  db.collection('tba').find({ slug: req.query.slug }).toArray()
 );
