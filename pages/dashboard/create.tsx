24027945179a4b6376ba46ccbc4388f5b1cae8b7
Ebrima <ebrima.jobe92@gmail.com>
2020-05-07 02:58:41 -0700

    remove sidebar from create page

--- pages/dashboard/create.tsx
+++ pages/dashboard/create.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { NextPage } from 'next';
 import { Create } from '../../src/components/Dashboard/Create/Create';
-import { SideBar } from '../../src/components/Dashboard/SideBar';
 import { Layout } from '../../src/components/Layout';
 
 const Page: NextPage = () => (
@@ -11,10 +10,7 @@ const Page: NextPage = () => (
     />
 
     <div className="mw8 center pv4 ph3" id="dashboard">
-      <section className="flex-m flex-l nl3-m nr3-m nl3-l nr3-l">
-        <SideBar />
-        <Create />
-      </section>
+      <Create />
     </div>
   </Layout>
 );
