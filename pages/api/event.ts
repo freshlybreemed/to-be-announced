838d033c2db32f3f90b5c015c7d042a463bf1859
Ebrima <ebrima.jobe92@gmail.com>
2020-05-08 12:35:53 -0700

    insert new event

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -1,6 +1,6 @@
-import { wrapAsync } from "./helpers";
-import { NextApiRequest } from "next";
+import { wrapAsync } from './helpers';
+import { NextApiRequest } from 'next';
 
 export default wrapAsync(async (req: NextApiRequest, db: any) =>
-  console.log(req.body)
+  db.collection('tba').insertOne(req.body),
 );
