1ba483a76b115f5c6491e2fc2127ac5b40491194
ebrima <ebrima.jobe92@gmail.com>
2020-05-10 15:34:51 -0700

    get events in getinitialprops

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,12 +1,27 @@
+import PropTypes from 'prop-types';
 import * as React from 'react';
 import { NextPage } from 'next';
 import { Events } from '../src/components/Events';
 import { Layout } from '../src/components/Layout';
+import axios from 'axios';
+import absoluteUrl from 'next-absolute-url';
 
-const Page: NextPage = () => (
+const Page: NextPage = (events) => (
   <Layout>
-    <Events />
+    <Events events={events} />
   </Layout>
 );
 
+Page.getInitialProps = async (ctx) => {
+  const { origin } = absoluteUrl(ctx.req);
+  const response = await axios.get(`${origin}/api/event`);
+  const result = response.data;
+  console.log(result);
+  return {
+    events: result,
+  };
+};
+Page.propTypes = {
+  events: PropTypes.object,
+};
 export default Page;
