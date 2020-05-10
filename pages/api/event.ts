9021cbc6227e6ff5e23c88ce36dbe2a0eef28678
ebrima <ebrima.jobe92@gmail.com>
2020-05-09 19:42:47 -0700

    event lambda get + post

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -1,6 +1,11 @@
 import { wrapAsync } from './helpers';
 import { NextApiRequest } from 'next';
 
-export default wrapAsync(async (req: NextApiRequest, db: any) =>
-  db.collection('tba').insertOne(req.body),
-);
+export default wrapAsync(async (req: NextApiRequest, db: any) => {
+  if (req.method === 'POST') {
+    return await db.collection('tba').insertOne(req.body);
+  }
+  if (req.method === 'GET') {
+    return await db.collection('tba').find().toArray();
+  }
+});
