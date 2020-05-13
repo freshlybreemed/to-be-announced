e5ab619f8a06f714c008469f74b5b3b988fe2ea8
ebrima <ebrima.jobe92@gmail.com>
2020-05-12 21:41:23 -0700

    delete events lambda

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -8,4 +8,7 @@ export default wrapAsync(async (req: NextApiRequest, db: any) => {
   if (req.method === 'GET') {
     return await db.collection('tba-event').find().toArray();
   }
+  if (req.method === 'DELETE') {
+    return await db.collection('tba-event').deleteMany();
+  }
 });
