064b30ca98be001ecfd66ebcd1d0b28438b43f86
Ebrima <ebrima.jobe92@gmail.com>
2020-05-20 11:12:34 -0700

    rich text editor

--- src/components/Dashboard/Create/TextEditor.tsx
+++ src/components/Dashboard/Create/TextEditor.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import { DraftailEditor, BLOCK_TYPE, INLINE_STYLE } from 'draftail';
 
 export const Editor: React.FunctionComponent = () => {
-  const initial = JSON.parse(window.sessionStorage.getItem('draftail:content'));
-
+  if (process.browser) {
+    console.log(JSON.parse(sessionStorage.getItem('draftail:content')));
+  }
   const onSave = (content) => {
     console.log('saving', content);
     window.sessionStorage.setItem('draftail:content', JSON.stringify(content));
@@ -12,7 +13,7 @@ export const Editor: React.FunctionComponent = () => {
 
   return (
     <DraftailEditor
-      rawContentState={initial || null}
+      rawContentState={null}
       onSave={onSave}
       blockTypes={[
         { type: BLOCK_TYPE.HEADER_THREE },
