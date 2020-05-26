9311c015cc283718e4cf5576b5192ccb2ef38565
Ebrima <ebrima.jobe92@gmail.com>
2020-05-25 20:49:10 -0700

    edit event page

---
+++ pages/dashboard/edit/[event].tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { NextPage } from 'next';
+import { Layout } from '../../../src/components/Layout/Layout';
+import axios from 'axios';
+import absoluteUrl from 'next-absolute-url';
+import PropTypes from 'prop-types';
+import { Create } from '../../../src/components/Dashboard/Create/Create';
+
+interface Props {
+  event: any;
+}
+
+const Page: NextPage<Props> = ({ event }) => (
+  <Layout>
+    <script
+      src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places`}
+    />
+
+    <div className="mw9 center pv2 ph3-ns overflow-hidden" id="dashboard">
+      <section className="flex-m flex-l nl3-m nr3-m nl3-l nr3-l">
+        <Create event={event} />
+      </section>
+    </div>
+  </Layout>
+);
+
+Page.getInitialProps = async (ctx) => {
+  const { origin } = absoluteUrl(ctx.req);
+  const { event } = ctx.query;
+  const response = await axios.get(`${origin}/api/event/${event}`);
+  const result = response.data;
+  console.log(result);
+  return {
+    event: result[0],
+  };
+};
+Page.propTypes = {
+  event: PropTypes.object,
+};
+
+export default Page;
