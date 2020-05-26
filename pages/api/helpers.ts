463805fe42069608cf76521e62d6dc28cadec8c3
ebrima <ebrima.jobe92@gmail.com>
2020-05-26 05:07:27 -0700

    mailgun function

--- pages/api/helpers.ts
+++ pages/api/helpers.ts
@@ -1,5 +1,8 @@
 const connect = require('./db');
 const cors = require('micro-cors')();
+const mailgun = require('mailgun-js');
+import { EventProps } from '../../src/@types/types';
+const DOMAIN = 'sandboxaf4a2cef857b4556a257062d0ed5309a.mailgun.org';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export const wrapAsync = (handler: any) => async (
@@ -19,3 +22,38 @@ export const wrapAsync = (handler: any) => async (
       .catch((error: any) => res.status(500).json({ error: error.message }))
   );
 };
+
+export const sendEmail = async (
+  email: string[],
+  event: EventProps,
+  order: any
+) => {
+  try {
+    const mg = mailgun({
+      apiKey: process.env.MAILGUN,
+      domain: DOMAIN,
+    });
+
+    for (var key in email) {
+      const data = {
+        from: `"TBA" <info@whatstba.com>`, // sender address
+        to: email[key],
+        subject: 'Hello',
+        text: 'Testing some Mailgun awesomness!',
+        template: 'test123',
+        'v:event': { event },
+        'v:order': order,
+      };
+      await mg.messages().send(data, function (error, body) {
+        if (error) console.log(error);
+        console.log(body);
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const stripeSecret = dev
+  ? process.env.STRIPE_SECRET_DEV
+  : process.env.STRIPE_SECRET_PROD;
