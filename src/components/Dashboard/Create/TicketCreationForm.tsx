517283b162c6ebeca1dd06e18b44856364f66e79
ebrima <ebrima.jobe92@gmail.com>
2020-05-26 04:53:02 -0700

    add labels to checkouts. remove placeholders

--- src/components/Dashboard/Create/TicketCreationForm.tsx
+++ src/components/Dashboard/Create/TicketCreationForm.tsx
@@ -15,51 +15,52 @@ export const TicketCreationForm: React.FunctionComponent<TicketingProps> = ({
   ticket,
 }) => {
   const [ticketName, setTicketName] = useState<string>(
-    ticket ? ticket.ticketName : '',
+    ticket ? ticket.ticketName : ''
   );
   const [quantity, setQuantity] = useState<number>(
-    ticket ? ticket.quantity : 0,
+    ticket ? ticket.quantity : 0
   );
   const [price, setPrice] = useState<number>(ticket ? ticket.price : 0);
   const [description, setDescription] = useState<string>(
-    ticket ? ticket.description : '',
+    ticket ? ticket.description : ''
   );
   console.log({ ticket, ticketName, quantity, description });
   return (
-    <>
-      {' '}
+    <div className="mw6 center">
       <div className="mv3">
+        <label className="f5-ns f6 fw7-ns fw5 db tl">Ticket Name</label>
+
         <input
           value={ticketName}
           onChange={(event) => {
             setTicketName(event.currentTarget.value);
           }}
-          className="pa2 bt-0 br-0 bl-0 input-reset  bb bg-black white mr3  w-75-ns w-100"
-          placeholder="Ticket Name"
+          className="pa2 bt-0 br-0 bl-0 input-reset  bb bg-black white mr3  w-100"
         />
       </div>
       <div className="mv3">
+        <label className="f5-ns f6 fw7-ns fw5 db tl">Ticket Quantity</label>
         <input
           value={quantity}
           onChange={(event) => {
             setQuantity(parseInt(event.currentTarget.value));
           }}
-          className="pa2 bt-0 br-0 bl-0 input-reset bb bg-black white mr3  w-75-ns w-100"
-          placeholder="Ticket Quantity"
+          className="pa2 bt-0 br-0 bl-0 input-reset bb bg-black white mr3  w-100"
         />
       </div>
       <div className="mv3">
+        <label className="f5-ns f6 fw7-ns fw5 db tl">Ticket Description</label>
         <input
           value={description}
           onChange={(event) => {
             setDescription(event.currentTarget.value);
           }}
-          className="pa2 bt-0 br-0 bl-0 input-reset bb bg-black white mr3  w-75-ns w-100"
-          placeholder="Ticket Description"
+          className="pa2 bt-0 br-0 bl-0 input-reset bb bg-black white mr3  w-100"
         />
       </div>
       <div className="mv3">
         {/* <span>$</span> */}
+        <label className="f5-ns f6 fw7-ns fw5 db tl">Price</label>
         <Cleave
           style={{ boxSizing: 'initial' }}
           value={price}
