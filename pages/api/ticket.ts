ad6edd4b260f7a413d173f492524bf4e81e67d6c
ebrima <ebrima.jobe92@gmail.com>
2020-05-26 14:28:59 -0700

    email template function

--- pages/api/ticket.ts
+++ pages/api/ticket.ts
@@ -1,8 +1,9 @@
 import { wrapAsync, sendEmail } from './helpers';
+import { ticketEmail } from './emailTemplates';
 import { NextApiRequest } from 'next';
 
 export default wrapAsync(async (req: NextApiRequest) => {
   const { event, order } = req.body;
   console.log(req.body);
-  return await sendEmail([order.emailAddress], event, order);
+  return await sendEmail([order.emailAddress], ticketEmail, event, order);
 });
