724066a9a57c8dc5f917b5804960d0cddcdb383a
Ebrima <ebrima.jobe92@gmail.com>
2020-05-25 20:48:18 -0700

    render live or sale ended text on my events

--- src/components/Dashboard/ManageEvent/MyEvents.tsx
+++ src/components/Dashboard/ManageEvent/MyEvents.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import classnames from 'classnames';
 import { formatDate, formatPrice } from '../../../lib';
 
 interface EventProps {
@@ -12,34 +13,44 @@ export const MyEvents: React.FunctionComponent<EventProps> = ({ events }) => {
         <h1 className="f1-ns f2 mt0">Manage Events</h1>
       </div>
       <main className="mw8 ml4-ns center">
-        {events.map((curr) => (
-          <article className="dt w-90 bb b--gray pb2 mt2">
-            <div className="dtc w2 w3-ns v-mid">
-              <img src={curr.image} className="db h3" />
-            </div>
-            <div className="dtc v-mid pl3">
-              <h1 className="f6 f5-ns fw7 lh-title mv0 underline-hover">
-                <a
-                  className="white no-underline"
-                  href={`/dashboard/manage/${curr.slug}`}
+        {events.map((curr) => {
+          const live = new Date(curr.startDate) > new Date();
+          return (
+            <article className="dt w-90 bb b--gray pb2 mt2">
+              <div className="dtc w2 w3-ns v-mid">
+                <img src={curr.image} className="db h3" />
+              </div>
+              <div className="dtc v-mid pl3">
+                <h1 className="f6 f5-ns fw7 lh-title mv0 underline-hover">
+                  <a
+                    className="white no-underline"
+                    href={`/dashboard/manage/${curr.slug}`}
+                  >
+                    {curr.name}
+                  </a>
+                </h1>
+                <h2 className="f6 fw6 mt0 mb0 gray">
+                  {formatDate(new Date(curr.startDate))}
+                </h2>
+                <h2
+                  className={`f6 fw6 mt0 mb0 ${classnames({
+                    green: live,
+                    red: !live,
+                  })}`}
                 >
-                  {curr.name}
-                </a>
-              </h1>
-              <h2 className="f6 fw6 mt0 mb0 gray">
-                {formatDate(new Date(curr.startDate))}
-              </h2>
-              <h2 className="f6 fw6 mt0 mb0 green">• Live</h2>
-            </div>
-            <div className="dtc v-mid tr">
-              <h1 className="f6 f5-ns fw7 lh-title mv0">
-                {formatPrice('58')} Total
-              </h1>
-              <h1 className="f6 f5-ns fw7 lh-title gray mv0">7 Tixs Sold</h1>
-              {/* <h2 className="f6 fw6 mt0 mb0 gray">Los Angeles</h2> */}
-            </div>
-          </article>
-        ))}
+                  • {live ? `Live` : `Sale Ended`}
+                </h2>
+              </div>
+              <div className="dtc v-mid tr">
+                <h1 className="f6 f5-ns fw7 lh-title mv0">
+                  {formatPrice('58')} Total
+                </h1>
+                <h1 className="f6 f5-ns fw7 lh-title gray mv0">7 Tixs Sold</h1>
+                {/* <h2 className="f6 fw6 mt0 mb0 gray">Los Angeles</h2> */}
+              </div>
+            </article>
+          );
+        })}
         <article className="dt w-90 bb b--gray pb2 mt2">
           <div className="dtc w2 w3-ns v-mid">
             <img
