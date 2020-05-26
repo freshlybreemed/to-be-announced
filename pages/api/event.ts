2fdae1658f193fa4ba39eb4be4258cc0a9283e15
ebrima <ebrima.jobe92@gmail.com>
2020-05-26 04:50:18 -0700

    update event db w/ upserts

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -3,7 +3,9 @@ import { NextApiRequest } from 'next';
 
 export default wrapAsync(async (req: NextApiRequest, db: any) => {
   if (req.method === 'POST') {
-    return await db.collection('tba-event').insertOne(req.body);
+    return await db
+      .collection('tba-event')
+      .updateOne({ slug: req.body.slug }, { $set: req.body }, { upsert: true });
   }
   if (req.method === 'GET') {
     return await db.collection('tba-event').find().toArray();
