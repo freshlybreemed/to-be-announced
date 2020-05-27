89a11e829dca31357b16ceea6a0b4bdc1e85c41b
ebrima <ebrima.jobe92@gmail.com>
2020-05-27 10:48:01 -0700

    update tix count lambda

--- pages/api/ticket.ts
+++ pages/api/ticket.ts
@@ -1,9 +1,31 @@
 import { wrapAsync, sendEmail } from './helpers';
+import { OrderProps, EventProps } from '../../src/@types/types';
 import { ticketEmail } from './emailTemplates';
 import { NextApiRequest } from 'next';
 
-export default wrapAsync(async (req: NextApiRequest) => {
+const updateTixCount = async (
+  order: OrderProps,
+  event: EventProps,
+  db: any
+) => {
+  let ops = Object.keys(order.cart).map((curr) => {
+    return {
+      updateOne: {
+        filter: { slug: event.slug },
+        update: {
+          $inc: {
+            [`ticketTypes.${curr}.sold`]: order.cart[curr].quantity,
+          },
+        },
+      },
+    };
+  });
+  console.log(ops);
+  return await db.collection('tba-event').bulkWrite(ops);
+};
+
+export default wrapAsync(async (req: NextApiRequest, db: any) => {
   const { event, order } = req.body;
-  console.log(req.body);
+  await updateTixCount(order, event, db);
   return await sendEmail([order.emailAddress], ticketEmail, event, order);
 });
