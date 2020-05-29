40ad1e498ee0a223069d83ec1177eef2a618733b
Ebrima <ebrima.jobe92@gmail.com>
2020-05-29 00:40:03 -0700

    unused parameter fix

--- pages/api/myevents.ts
+++ pages/api/myevents.ts
@@ -1,12 +1,11 @@
 import { wrapAsync } from './helpers';
 import { NextApiRequest } from 'next';
 
-export default wrapAsync(
-  async (req: NextApiRequest, db: any) =>
-    await db
-      .collection('tba-event')
-      .find({
-        organizerId: '123',
-      })
-      .toArray(),
-);
+export default wrapAsync(async (_req: NextApiRequest, db: any) => {
+  return await db
+    .collection('tba-event')
+    .find({
+      organizerId: '123',
+    })
+    .toArray();
+});
