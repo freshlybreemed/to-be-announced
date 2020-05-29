d53fe5514ec1db250c27b56e70ebab5a160f81ec
Ebrima <ebrima.jobe92@gmail.com>
2020-05-29 00:31:45 -0700

    find active events lambda

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -3,12 +3,15 @@ import { NextApiRequest } from 'next';
 
 export default wrapAsync(async (req: NextApiRequest, db: any) => {
   if (req.method === 'POST') {
+  if (req.method === 'GET') {
     return await db
       .collection('tba-event')
-      .updateOne({ slug: req.body.slug }, { $set: req.body }, { upsert: true });
-  }
-  if (req.method === 'GET') {
-    return await db.collection('tba-event').find().toArray();
+      .find({
+        startDate: {
+          $gte: new Date(),
+        },
+      })
+      .toArray();
   }
   if (req.method === 'DELETE') {
     return await db.collection('tba-event').deleteMany();
