3985b7573b573e08abf4252b08ae7e1e316d5ab7
Ebrima <ebrima.jobe92@gmail.com>
2020-05-28 18:15:27 -0700

    stop emails

--- pages/api/ticket.ts
+++ pages/api/ticket.ts
@@ -1,12 +1,15 @@
-import { wrapAsync, sendEmail } from './helpers';
+import {
+  wrapAsync,
+  // sendEmail
+} from './helpers';
 import { OrderProps, EventProps } from '../../src/@types/types';
-import { ticketEmail } from './emailTemplates';
+// import { ticketEmail } from './emailTemplates';
 import { NextApiRequest } from 'next';
 
 const updateTixCount = async (
   order: OrderProps,
   event: EventProps,
-  db: any
+  db: any,
 ) => {
   let ops = Object.keys(order.cart).map((curr) => {
     return {
