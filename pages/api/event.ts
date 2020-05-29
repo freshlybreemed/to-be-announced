b88824580adbc9f88f531380886b1c6b12cb3471
Ebrima <ebrima.jobe92@gmail.com>
2020-05-29 00:32:27 -0700

    retain Date types , auto set organizerId on event mongo updates

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -1,8 +1,23 @@
 import { wrapAsync } from './helpers';
 import { NextApiRequest } from 'next';
+import { EventProps } from '../../src/@types/types';
 
 export default wrapAsync(async (req: NextApiRequest, db: any) => {
   if (req.method === 'POST') {
+    const event: EventProps = req.body;
+    return await db.collection('tba-event').updateOne(
+      { slug: req.body.slug },
+      {
+        $set: {
+          ...req.body,
+          organizerId: '123',
+          startDate: new Date(event.startDate),
+          endDate: new Date(event.endDate),
+        },
+      },
+      { upsert: true },
+    );
+  }
   if (req.method === 'GET') {
     return await db
       .collection('tba-event')
