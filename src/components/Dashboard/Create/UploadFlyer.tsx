3186851f6329bd99f2e2ea04341bf5d41b413478
freshly <ebrima.jobe92@gmail.com>
2020-06-06 04:55:02 -0700

    upload button input absolute

--- src/components/Dashboard/Create/UploadFlyer.tsx
+++ src/components/Dashboard/Create/UploadFlyer.tsx
@@ -48,32 +48,32 @@ export const UploadFlyer: React.FunctionComponent<EventImageProps> = ({
   };
 
   return (
-    <div className="mt4 b--white hover-bg-white hover-black dib noselect br-100 b--solid pa1 ph3 f5 fw5 ">
+    <div className="mt4 relative overflow-hidden dib">
+      <div className="b--white hover-bg-white bg-black white hover-black dib  br-100 b--solid pa1 ph3 f5 fw5">
+        {' '}
+        <i aria-label="icon: plus" className="center">
+          <svg
+            viewBox="64 64 896 896"
+            focusable="false"
+            data-icon="plus"
+            width="1em"
+            height="1em"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8z"></path>
+            <path d="M176 474h672q8 0 8 8v60q0 8-8 8H176q-8 0-8-8v-60q0-8 8-8z"></path>
+          </svg>
+        </i>
+        <br />
+        Upload Flyer
+      </div>
       <input
-        style={{ cursor: 'pointer' }}
         type="file"
-        className="overflow-hidden o-0 absolute"
         onChange={(e) => handleChange(e.target.files)}
+        style={{ left: 0, top: 0 }}
+        className="absolute o-0"
       />
-      <span className="w-100">
-        <div>
-          <i aria-label="icon: plus" className="center">
-            <svg
-              viewBox="64 64 896 896"
-              focusable="false"
-              data-icon="plus"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8z"></path>
-              <path d="M176 474h672q8 0 8 8v60q0 8-8 8H176q-8 0-8-8v-60q0-8 8-8z"></path>
-            </svg>
-          </i>
-          <div className="">Upload Event Image</div>
-        </div>
-      </span>
     </div>
   );
 };
