a6db6df9b45ae2c08eba33cd8537a43ffd63f134
ebrima <ebrima.jobe92@gmail.com>
2020-06-12 00:39:37 -0700

    resize date time svgs

--- src/components/Event/Event.tsx
+++ src/components/Event/Event.tsx
@@ -49,11 +49,11 @@ export const Event: React.FunctionComponent<EventViewProps> = ({ event }) => {
               className="f4-ns f5 fw6 lh-title mv1 center-s underline-hover white"
             >
               <tr>
-                <td rowSpan={2} className="mt0 pt0 v-top">
+                <td rowSpan={2} className="mt0 pt1 v-top">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
+                    width="18"
+                    height="18"
                     className="pr1  "
                     fill="currentColor"
                     viewBox="0 0 24 24"
@@ -90,10 +90,11 @@ export const Event: React.FunctionComponent<EventViewProps> = ({ event }) => {
                 <td rowSpan={2} className="mt0 v-top">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
+                    width="15"
+                    height="15"
                     className="pr1 "
                     fill="currentColor"
+                    viewBox="0 0 24 24"
                   >
                     <path d="M20 20h-4v-4h4v4zm-6-10h-4v4h4v-4zm6 0h-4v4h4v-4zm-12 6h-4v4h4v-4zm6 0h-4v4h4v-4zm-6-6h-4v4h4v-4zm16-8v22h-24v-22h3v1c0 1.103.897 2 2 2s2-.897 2-2v-1h10v1c0 1.103.897 2 2 2s2-.897 2-2v-1h3zm-2 6h-20v14h20v-14zm-2-7c0-.552-.447-1-1-1s-1 .448-1 1v2c0 .552.447 1 1 1s1-.448 1-1v-2zm-14 2c0 .552-.447 1-1 1s-1-.448-1-1v-2c0-.552.447-1 1-1s1 .448 1 1v2z" />
                   </svg>
