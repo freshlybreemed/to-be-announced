4a7ea9bbd4f4c4120d81327fb2e602ce3a9a5063
Ebrima <ebrima.jobe92@gmail.com>
2020-06-21 21:25:27 -0700

    staticPaths + staticProps test

--- pages/dashboard/manage/[event].tsx
+++ pages/dashboard/manage/[event].tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import { NextPage } from 'next';
+import { NextPage, GetStaticProps, GetStaticPaths } from 'next';
 import { ManageEvent } from '../../../src/components/Dashboard';
 import { Layout } from '../../../src/components/Layout';
 import axios from 'axios';
-import absoluteUrl from 'next-absolute-url';
 import { EventProps } from '../../../src/@types/types';
 
 interface Props {
@@ -23,16 +22,31 @@ const Page: NextPage<Props> = ({ event }) => (
     </div>
   </Layout>
 );
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Get the paths we want to pre-render based on users
+  const response = await axios.get(`https://whatstba.com/api/event`);
+  const paths = response.data.map((event) => {
+    return { params: { event: event.slug } };
+  });
 
-Page.getInitialProps = async (ctx) => {
-  const { origin } = absoluteUrl(ctx.req);
-  const { event } = ctx.query;
-  const response = await axios.get(`${origin}/api/event/${event}`);
-  const result = response.data;
-  console.log(result);
-  return {
-    event: result[0],
-  };
+  console.log(paths);
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: true };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  try {
+    console.log('try', params);
+    const event = (
+      await axios.get(`https://whatstba.com/api/event/${params.event}`)
+    ).data[0];
+    return { props: { event } };
+  } catch (err) {
+    console.log('err', err);
+    return { props: { errors: '' } };
+  }
 };
 
 export default Page;
