00f12f4b05b90c923deccb1d232436083c027156
ebrima <ebrima.jobe92@gmail.com>
2020-06-25 02:04:03 -0700

    render venue details from obj

--- src/components/Venues/Venues.tsx
+++ src/components/Venues/Venues.tsx
@@ -8,6 +8,50 @@ function useMounted() {
   useEffect(() => setMounted(true), []);
   return mounted;
 }
+const obj = {
+  venueName: 'Howard Theatre',
+  capacity: 200,
+  contactInfo: {
+    name: 'Ebrima Jobe',
+    email: 'freshlybreemed@gmail.com',
+  },
+  location: {
+    address: '189 The Grove Dr',
+    city: 'Los Angeles',
+    lat: 34.072225,
+    lng: -118.3581333,
+    placeId: 'ChIJwwPTwi-5woARbtuI_49vkMM',
+    state: 'CA',
+    timeZoneId: 'America/Los_Angeles',
+    venue: 'The Grove',
+    zip: '90036',
+  },
+  socialLinks: {
+    facebook: 'www.facebook.com/freshlybreemed',
+    instgram: 'www.instagram.com/howardtheatre',
+  },
+  photoGallery: [
+    'https://dice-media.imgix.net/attachments/2020-01-15/4bed9553-1604-4ebf-976d-5dfd03f12666.jpg?rect=0%2C81%2C2048%2C1118',
+    'https://dice-media.imgix.net/attachments/2020-01-15/b7a5dd28-2559-4af6-a0c4-35fcc4a13a03.jpg',
+  ],
+  upcomingEvents: [
+    {
+      image:
+        'https://dice-media.imgix.net/attachments/2020-03-10/aab83992-8fe3-4954-8eef-b720e286544e.jpg?rect=160%2C0%2C640%2C640?auto=compress&lossless=true&auto=format&h=250&w=250&dpr=2',
+      title: 'Saturday Night Live',
+      startDate: new Date(),
+      slug: 'brozay',
+    },
+    {
+      image:
+        'https://dice-media.imgix.net/attachments/2020-02-26/4b1afb5b-a4dd-485b-ba5b-faa3eb5381c9.jpg?rect=241%2C225%2C1413%2C1413?auto=compress&lossless=true&auto=format&h=250&w=250&dpr=2',
+      title: 'Party & Bullshit',
+      startDate: new Date(),
+      slug: 'brozay',
+    },
+  ],
+};
+
 export const Venues: React.FunctionComponent = () => {
   const isMounted = useMounted();
 
@@ -17,9 +61,9 @@ export const Venues: React.FunctionComponent = () => {
   });
   return (
     <div className={'relative'}>
-      <div className="ph4 ttu fw5 f2">Howard Theatre</div>
+      <div className="ph4 ttu fw5 f2">{obj.venueName}</div>
       <img
-        src="https://dice-media.imgix.net/attachments/2020-01-15/4bed9553-1604-4ebf-976d-5dfd03f12666.jpg?rect=0%2C81%2C2048%2C1118"
+        src={obj.photoGallery[0]}
         className="w-90 relative"
         style={{ bottom: '-20px' }}
       />
@@ -27,31 +71,39 @@ export const Venues: React.FunctionComponent = () => {
         <div className="pb4">
           <h3 className="mw8 ttu fw5">Information</h3>
           <div className="fw6 f5 pv1">Capacity</div>
-          <div className="fw7 f4">200</div>
+          <div className="fw7 f4">{obj.capacity}</div>
         </div>
         <div className="pb4">
           <div className="fw6 f5 pv1">Contact details</div>
-          <div className="fw7 f4">freshlybreemed@gmail.com</div>
+          <div className="fw7 f4">{obj.contactInfo.email}</div>
         </div>
         <div className="pb4">
           <div className="fw6 f5 pv1">Address</div>
-          <div className="fw7 f4">202 T St. NW, DC 20002</div>
+          <div className="fw7 f4">{`${obj.location.address} ${obj.location.city}, ${obj.location.state}`}</div>
         </div>
         <div className="pb4">
           <div className="fw6 f5 pv1">Social Links</div>
-          <div className="fw7 f4">Facebook</div>
-          <div className="fw7 f4">Instagram</div>
+          <a
+            href={obj.socialLinks.facebook}
+            className="fw7 f4 black no-underline"
+          >
+            Facebook
+          </a>
+          <a
+            href={obj.socialLinks.instgram}
+            className="fw7 f4 black no-underline"
+          >
+            Instagram
+          </a>
         </div>
       </div>
 
       <div className={'w-100 ph4  pt4 bg-black white'}>
         <div className="pb4">
           <h3 className="mw8 ttu fw5">Photo Gallery</h3>
-
-          <img
-            src="https://dice-media.imgix.net/attachments/2020-01-15/b7a5dd28-2559-4af6-a0c4-35fcc4a13a03.jpg"
-            className="w-60"
-          />
+          {obj.photoGallery.map((curr) => {
+            return <img src={curr} className="w-60" />;
+          })}
         </div>
       </div>
       <div className={'w-100 ph4  pt4 bg-white black'}>
@@ -78,91 +130,42 @@ export const Venues: React.FunctionComponent = () => {
                 boxSizing: 'inherit',
               }}
             >
-              <div className="w-100 ">
-                <div
-                  className="w-100 h-100 relative flex"
-                  style={{ boxSizing: 'inherit' }}
-                >
-                  <div
-                    style={{
-                      backgroundImage:
-                        'url("https://dice-media.imgix.net/attachments/2020-02-26/4b1afb5b-a4dd-485b-ba5b-faa3eb5381c9.jpg?rect=241%2C225%2C1413%2C1413?auto=compress&lossless=true&auto=format&h=250&w=250&dpr=2")',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center center',
-                      top: '0px',
-                      left: '0px',
-                      width: '300px',
-                      height: '250px',
-                    }}
-                    className="o-100  "
-                  ></div>
-                </div>
+              {obj.upcomingEvents.map((curr) => {
+                return (
+                  <div className="w-100 ">
+                    <div
+                      className="w-100 h-100 relative flex"
+                      style={{ boxSizing: 'inherit' }}
+                    >
+                      <div
+                        style={{
+                          backgroundImage:
+                            'url("https://dice-media.imgix.net/attachments/2020-02-26/4b1afb5b-a4dd-485b-ba5b-faa3eb5381c9.jpg?rect=241%2C225%2C1413%2C1413?auto=compress&lossless=true&auto=format&h=250&w=250&dpr=2")',
+                          backgroundSize: 'cover',
+                          backgroundPosition: 'center center',
+                          top: '0px',
+                          left: '0px',
+                          width: '300px',
+                          height: '250px',
+                        }}
+                        className="o-100  "
+                      ></div>
+                    </div>
 
-                <h4 className="f5 fw4 mb0">{`Thu 9 Jul — 20:00`}</h4>
-                <a href="" className="black no-underline">
-                  <h4 className="f4 fw8 mt2 mb0 ttu">{`Party & Bullshit`}</h4>
-                </a>
-                <h4 className="f5 fw6 mt1 pt1 ">{`Howard Theatre`}</h4>
-                <a className=" bg-black white br-100 pa2 tc f4-ns f6 fw6-ns fw5 grow noselect no-underline ph4">
-                  Get Tickets
-                </a>
-              </div>
-              <div className="w-100 ">
-                <div
-                  className="w-100 h-100 relative flex"
-                  style={{ boxSizing: 'inherit' }}
-                >
-                  <div
-                    style={{
-                      backgroundImage:
-                        'url("https://dice-media.imgix.net/attachments/2020-02-05/6ad52558-ee29-4574-93ba-de5cbdbc6b59.jpg")',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center center',
-                      top: '0px',
-                      left: '0px',
-                      width: '300px',
-                      height: '250px',
-                    }}
-                    className="o-100  "
-                  ></div>
-                </div>
-                <h4 className="f5 fw4 mb0">{`Fri 10 Jul — 20:00`}</h4>
-                <a href="" className="black no-underline">
-                  <h4 className="f4 fw8 mt2 mb0 ttu">{`Kill Moe`}</h4>
-                </a>
-                <h4 className="f5 fw6 mt1 pt1 ">{`Howard Theatre`}</h4>
-                <a className="bg-black white br-100 pa2 tc f4-ns f6 fw6-ns fw5 grow noselect ph4 no-underline">
-                  Get Tickets
-                </a>
-              </div>
-              <div className="w-100 ">
-                <div
-                  className="w-100 h-100 relative flex"
-                  style={{ boxSizing: 'inherit' }}
-                >
-                  <div
-                    style={{
-                      backgroundImage:
-                        'url("https://dice-media.imgix.net/attachments/2020-03-10/aab83992-8fe3-4954-8eef-b720e286544e.jpg?rect=160%2C0%2C640%2C640?auto=compress&lossless=true&auto=format&h=250&w=250&dpr=2")',
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center center',
-                      top: '0px',
-                      left: '0px',
-                      width: '300px',
-                      height: '250px',
-                    }}
-                    className="o-100  "
-                  ></div>
-                </div>
-                <h4 className="f5 fw4 mb0">{`Sun 16 Jul — 10:00`}</h4>
-                <a href="" className="black no-underline">
-                  <h4 className="f4 fw8 mt2 mb0 ttu">{`Ice Cream Sunday`}</h4>
-                </a>
-                <h4 className="f5 fw6 mt1 pt1 ">{`Howard Theatre`}</h4>
-                <a className="bg-black white br-100 pa2 tc f4-ns f6 fw6-ns fw5 grow noselect ph4 no-underline">
-                  Get Tickets
-                </a>
-              </div>
+                    <h4 className="f5 fw4 mb0">{curr.startDate.toString()}</h4>
+                    <a href="" className="black no-underline">
+                      <h4 className="f4 fw8 mt2 mb0 ttu">{curr.title}</h4>
+                    </a>
+                    <h4 className="f5 fw6 mt1 pt1 ">{obj.venueName}</h4>
+                    <a
+                      href={`/${curr.slug}`}
+                      className=" bg-black white br-100 pa2 tc f4-ns f6 fw6-ns fw5 grow noselect no-underline ph4"
+                    >
+                      Get Tickets
+                    </a>
+                  </div>
+                );
+              })}
             </div>
           )}
         </div>
