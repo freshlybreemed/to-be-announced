d618bf52f1b93eb490825c4e80f9a2c9b4c4ae12
ebrima <ebrima.jobe92@gmail.com>
2020-06-25 15:22:05 -0700

    seo and ga analytics

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,10 +1,11 @@
-export const DEV = process.env.NODE_ENV !== 'production'
+export const DEV = process.env.NODE_ENV !== 'production';
 
-export const GA_TRACKING_ID = ''
-export const FB_TRACKING_ID = ''
-export const SENTRY_TRACKING_ID = ''
+export const GA_TRACKING_ID = 'UA-170856828-1';
+export const FB_TRACKING_ID = '';
+export const SENTRY_TRACKING_ID = '';
 
-export const SITE_NAME = ''
-export const SITE_TITLE = ''
-export const SITE_DESCRIPTION = ''
-export const SITE_IMAGE = ''
+export const SITE_NAME = '';
+export const SITE_TITLE = 'Social Ticketing';
+export const SITE_DESCRIPTION = '';
+export const SITE_IMAGE =
+  'https://res.cloudinary.com/dzsf703vh/image/upload/v1593116407/wlx9lujbtxc96m9nkfvy.png';
