a8ea760f05f4625f7f5ab14ba069a71518e2a7d5
freshly <ebrima.jobe92@gmail.com>
2020-06-26 03:06:09 -0700

    myEvents page  SWR

--- pages/dashboard/myevents.tsx
+++ pages/dashboard/myevents.tsx
@@ -2,30 +2,29 @@ import * as React from 'react';
 import { NextPage } from 'next';
 import { MyEvents } from '../../src/components/Dashboard/';
 import { Layout } from '../../src/components/Layout/';
-import axios from 'axios';
-import absoluteUrl from 'next-absolute-url';
 import { EventProps } from '../../src/@types/types';
+import useRequest from '../../src/lib/useRequest';
 
-interface Props {
-  events: EventProps[];
-}
-const Page: NextPage<Props> = ({ events }) => (
+
+const Page: NextPage = () => {
+  const { data }  = useRequest<EventProps[]>({ url: `/api/event` })
+  return(
   <Layout>
     <div>
       <div className="mw8-ns w-100 center pv4 ph3-ns" id="dashboard">
-        <MyEvents events={events} />
+      {data ? (
+        <MyEvents events={data} />   ) : (
+          <div className="vh-50 dt center">
+            <div className="v-mid dtc">
+              <i className="fa-6x fa fa-spinner fa-spin mr2" />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   </Layout>
-);
+)
+  }
 
-Page.getInitialProps = async (ctx) => {
-  const { origin } = absoluteUrl(ctx.req);
-  const response = await axios.get(`${origin}/api/myevents`);
-  const result = response.data;
-  return {
-    events: result,
-  };
-};
 
 export default Page;
