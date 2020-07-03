2b8363f44e70d3c6582a5fe0afb9c1ac3c8bdf23
ebrima <ebrima.jobe92@gmail.com>
2020-07-03 01:06:01 -0700

    site name default props

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -4,8 +4,8 @@ export const GA_TRACKING_ID = 'UA-170856828-1';
 export const FB_TRACKING_ID = '';
 export const SENTRY_TRACKING_ID = '';
 
-export const SITE_NAME = '';
+export const SITE_NAME = 'Social Ticketing';
 export const SITE_TITLE = 'Social Ticketing';
-export const SITE_DESCRIPTION = '';
+export const SITE_DESCRIPTION = 'Discover lit events.';
 export const SITE_IMAGE =
   'https://res.cloudinary.com/dzsf703vh/image/upload/v1593116407/wlx9lujbtxc96m9nkfvy.png';
