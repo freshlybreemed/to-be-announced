53aa8d7e11bbcc113c1e3ba7331f76c1edbeabdc
ebrima <ebrima.jobe92@gmail.com>
2020-07-03 01:04:23 -0700

    meta tags in layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,14 +2,29 @@ import * as React from 'react';
 import { Nav, Footer } from '.';
 import Head from 'next/head';
 import classnames from 'classnames';
+import {
+  SITE_NAME,
+  SITE_TITLE,
+  SITE_DESCRIPTION,
+  SITE_IMAGE,
+} from '../../constants/env';
 
 interface LayoutProps {
   noPadding?: boolean;
+  data?: {
+    title: string;
+    header: string;
+    description: string;
+  };
 }
-export const Layout: React.FunctionComponent<LayoutProps> = (props) => (
+export const Layout: React.FunctionComponent<LayoutProps> = ({
+  noPadding,
+  children,
+  data,
+}) => (
   <div
     className={`bg-black white ${classnames({
-      'pa4-ns pa3': !props.noPadding,
+      'pa4-ns pa3': !noPadding,
     })} relative`}
   >
     {/*language=PostCSS*/}
@@ -17,6 +32,24 @@ export const Layout: React.FunctionComponent<LayoutProps> = (props) => (
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={data ? data.header : SITE_NAME} />
+      <meta property="og:title" content={data ? data.title : SITE_TITLE} />
+      <title>{data ? data.title : SITE_TITLE}</title>
+      <meta
+        property="og:description"
+        content={data ? data.description : SITE_DESCRIPTION}
+      />
+      <meta property="og:image" content={SITE_IMAGE} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={SITE_NAME} />
+      <meta name="twitter:title" content={SITE_TITLE} />
+      <meta name="twitter:description" content={SITE_DESCRIPTION} />
+      <meta property="twitter:image" content={SITE_IMAGE} />
+      <meta
+        name="format-detection"
+        content="telephone=no, address=no, email=no"
       />
     </Head>
     <div
@@ -34,15 +67,15 @@ export const Layout: React.FunctionComponent<LayoutProps> = (props) => (
     />
     <div
       className={`${classnames({
-        'pa4-ns pa3': props.noPadding,
+        'pa4-ns pa3': noPadding,
       })}`}
     >
       <Nav />
     </div>
-    <main className="relative">{props.children}</main>
+    <main className="relative">{children}</main>
     <div
       className={`${classnames({
-        'pa4-ns pa3': props.noPadding,
+        'pa4-ns pa3': noPadding,
       })}`}
     >
       <Footer />
