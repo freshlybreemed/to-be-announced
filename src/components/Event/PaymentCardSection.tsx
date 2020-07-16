f6250424fca42d1c6251dd3dd79cedaf7d8c2aa5
ebrima <ebrima.jobe92@gmail.com>
2020-07-16 02:28:57 -0700

    payment card section

---
+++ src/components/Event/PaymentCardSection.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+// import { useState } from 'react';
+import 'cleave.js/dist/addons/cleave-phone.us';
+import { CardElement } from '@stripe/react-stripe-js';
+
+// Custom styling can be passed to options when creating an Element.
+const CARD_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      color: '#32325d',
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+        color: '#aab7c4',
+      },
+    },
+    invalid: {
+      color: '#fa755a',
+      iconColor: '#fa755a',
+    },
+  },
+};
+
+interface PaymentCheckoutProps {
+  handleChange: any;
+  cardRef: any;
+}
+
+export const PaymentCardSection: React.FunctionComponent<PaymentCheckoutProps> = ({
+  handleChange,
+  cardRef,
+}) => {
+  return (
+    <CardElement
+      onReady={cardRef}
+      id="card-element"
+      options={CARD_ELEMENT_OPTIONS}
+      onChange={handleChange}
+    />
+  );
+};
