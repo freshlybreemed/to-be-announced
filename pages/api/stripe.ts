0cd77f8d5d0631ff96dfd493e9f05e5c3d6221ef
ebrima <ebrima.jobe92@gmail.com>
2020-07-16 02:31:06 -0700

    paymentintent create vs stripe checkout

--- pages/api/stripe.ts
+++ pages/api/stripe.ts
@@ -7,35 +7,18 @@ const saveCheckoutSession = async (order: OrderProps, db: any) =>
   await db.collection('checkout').insertOne(order);
 
 const generateStripeCheckout = async (event: EventProps, order: OrderProps) => {
-  const tickets = Object.keys(order.cart)
-    .filter((curr) => order.cart[curr].price > 0)
-    .map((curr) => {
-      return {
-        name: `${event.name} - ${curr} ${
-          order.cart[curr].quantity > 1 ? `Tickets` : `Ticket`
-        }`,
-        amount: order.cart[curr].price * 112,
-        currency: 'usd',
-        quantity: order.cart[curr].quantity,
-        images: [event.image],
-      };
-    });
   const metaData = { slug: event.slug };
   for (var curr in order.cart) {
     metaData[curr] = order.cart[curr].quantity.toString();
   }
   console.log(metaData);
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    payment_intent_data: {
-      metadata: {
-        emailAddress: order.emailAddress,
-        ...metaData,
-      },
+  const session = await stripe.paymentIntents.create({
+    metadata: {
+      emailAddress: order.emailAddress,
+      ...metaData,
     },
-    line_items: tickets,
-    success_url: `https://whatstba.com/user`,
-    cancel_url: `https://whatstba.com/user/balance`,
+    currency: 'USD',
+    amount: order.total * 100,
   });
 
   return session;
