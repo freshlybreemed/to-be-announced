a026841adfac235c922f8e28e5b15e6194af8ccb
Ebrima <ebrima.jobe92@gmail.com>
2020-07-17 21:00:22 -0700

    clean up

--- src/components/Dashboard/Create/PlacesAutoComplete.tsx
+++ src/components/Dashboard/Create/PlacesAutoComplete.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import classnames from 'classnames';
 import axios from 'axios';
 import usePlacesAutocomplete, { getGeocode } from 'use-places-autocomplete';
 import useOnclickOutside from 'react-cool-onclickoutside';
