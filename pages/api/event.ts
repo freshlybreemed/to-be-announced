bb460d1cf633527719d51440f9d93feab4ebf243
ebrima <ebrima.jobe92@gmail.com>
2021-07-05 18:35:05 -0700

    algolia search backend update

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -1,31 +1,48 @@
 import { wrapAsync } from './helpers';
 import { NextApiRequest } from 'next';
 import { EventProps } from '../../src/@types/types';
+import algoliasearch from 'algoliasearch';
+
+
 
 export default wrapAsync(async (req: NextApiRequest, db: any) => {
-  if (req.method === 'POST') {
-    const event: EventProps = req.body;
-    return await db.collection('event').updateOne(
-      { slug: event.slug },
-      {
-        $set: {
-          ...req.body,
-          publishDate: new Date(event.publishDate),
-          startDate: new Date(event.startDate),
-          endDate: new Date(event.endDate),
-          updatedAt: new Date(),
+  switch ((req.method)) {
+    case "POST":
+      const event: EventProps = req.body;
+      const client = algoliasearch(process.env.ALGOLIA_ID, process.env.ALGOLIA_KEY);
+      const index = client.initIndex('events');
+      const algoliaEvent = {
+        ...event,
+        objectID: event._id
+      }
+      await index
+      .partialUpdateObject(algoliaEvent,
+        {
+          createIfNotExists: true
+        })
+      .catch((err)=>console.log('error',err));
+    
+      return await db.collection('event').updateOne(
+        { slug: event.slug },
+        {
+          $set: {
+            ...req.body,
+            publishDate: new Date(event.publishDate),
+            startDate: new Date(event.startDate),
+            endDate: new Date(event.endDate),
+            updatedAt: new Date(),
+          },
         },
-      },
-      { upsert: true },
-    );
-  }
-  if (req.method === 'GET') {
-    return await db
+        { upsert: true },
+      );
+    case "GET":
+      return await db
       .collection('event')
-      .find({ })
+      .find({})
       .toArray();
-  }
-  if (req.method === 'DELETE') {
-    return await db.collection('event').deleteMany();
+    case "DELETE":
+      return await db.collection('event').deleteMany();
+    default:
+      break;
   }
 });
